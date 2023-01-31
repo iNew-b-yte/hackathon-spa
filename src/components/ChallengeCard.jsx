20ab2dbc69974991227fbb7ffda0b220750cc155
Naveen <naveenbrothers26@gmail.com>
2023-01-31 13:13:40 +0530

    update code

--- src/components/ChallengeCard.jsx
+++ src/components/ChallengeCard.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const ChallengeCard = ({ id, heading, img_src, start_date, end_date }) => {
 
   const [status, setStatus] = useState();
+  const [classname, setClassName] = useState("");
 
   //start date
   const _month = new Date(start_date).getMonth();
@@ -26,12 +27,13 @@ const ChallengeCard = ({ id, heading, img_src, start_date, end_date }) => {
 
     if (today < startDate) {
       setStatus("Upcoming");
-    } else if (startDate === today) {
+      setClassName("bg-warning-subtle border-warning");
+    } else if (startDate === today || today < _end_date) {
       setStatus("Active");
-    } else if (today < _end_date) {
-      setStatus("Active");
+      setClassName("bg-success-subtle border-success");
     } else {
       setStatus(`Ended on ${end_date}`);
+      setClassName("bg-danger-subtle border-danger");
     }
 
     return () => {
@@ -46,7 +48,7 @@ const ChallengeCard = ({ id, heading, img_src, start_date, end_date }) => {
         <div className='bg-white rounded p-1'>
           <img className='card-img-top' src={img_src} alt="img-png" />
           <div className="card-body text-center">
-            <p id='status' className='d-inline-block px-2 custom-fontSize rounded-pill bg-success-subtle border border-successF'>{status}</p>
+            <p id='status' className={`d-inline-block px-2 custom-fontSize rounded-pill border ${classname}`}>{status}</p>
             <h1 className="card-title fs-6 mb-3">{heading}</h1>
             <button type='button' className='btn btn-sm rounded text-center card-button fw-bold px-4 w-75 custom-font-Size' >
               Participate Now
